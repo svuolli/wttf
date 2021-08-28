0c410351e0bd97fc827d4594a20517722bd0f715
Sami Vuolli <sami.vuolli@gmail.com>
2021-08-28 20:02:00 +0200

    Flatten curves

--- src/ttfparse.cpp
+++ src/ttfparse.cpp
@@ -117,7 +117,8 @@ class shape
 
     void add_vertex(float x, float y, bool on_curve)
     {
-        m_flat |= !on_curve;
+        TTF_ASSERT(on_curve || !empty());
+        m_flat &= on_curve;
         m_contours.back().push_back({x, y, on_curve});
     }
 
@@ -169,7 +170,95 @@ class shape
         }
     }
 
+    shape flatten(float const flatness) const
+    {
+        if(m_flat) return *this;
+
+        auto result = shape{
+            m_min_x, m_min_y, m_max_x, m_max_y, m_contours.size()};
+
+        for(auto & cont: m_contours)
+        {
+            result.add_contour(cont.size());
+            auto prev_on_curve = true;
+            auto cx = 0.0f;
+            auto cy = 0.0f;
+            auto ex = 0.0f;
+            auto ey = 0.0f;
+
+            for(auto & v: cont)
+            {
+                if(v.on_curve)
+                {
+                    if(prev_on_curve)
+                    {
+                        result.add_vertex(v.x, v.y, true);
+                    }
+                    else
+                    {
+                        result.add_tesselated_curve(
+                            flatness, ex, ey, cx, cy, v.x, v.y);
+                    }
+
+                    ex = v.x;
+                    ey = v.y;
+                }
+                else
+                {
+                    if(!prev_on_curve)
+                    {
+                        auto const nx = (v.x+cx)/2.0f;
+                        auto const ny = (v.y+cy)/2.0f;
+                        result.add_tesselated_curve(
+                            flatness, ex, ey, cx, cy, nx, ny);
+                        ex = nx;
+                        ey = ny;
+                    }
+
+                    cx = v.x;
+                    cy = v.y;
+                }
+                prev_on_curve = v.on_curve;
+            }
+
+            if(!prev_on_curve)
+            {
+                auto & v = cont[0];
+                result.add_tesselated_curve(
+                    flatness, ex, ey, cx, cy, v.x, v.y, false);
+            }
+        }
+
+        return result;
+    }
+
     private:
+    void add_tesselated_curve(
+        float flatness,
+        float x0, float y0,
+        float x1, float y1,
+        float x2, float y2,
+        bool add_last_point = true)
+    {
+        // Middle of curve
+        auto const mx = (x0 + 2.0f*x1 + x2) / 4.0f;
+        auto const my = (y0 + 2.0f*y1 + y2) / 4.0f;
+
+        // Vector from middle of curve to direct line
+        auto const dx = (x0+x2)/2.0f - mx;
+        auto const dy = (y0+y2)/2.0f - my;
+
+        if(dx*dx+dy*dy > flatness)
+        {
+            add_tesselated_curve(flatness, x0, y0, (x0+x1)/2.0f, (y0+y1)/2.0f, mx, my);
+            add_tesselated_curve(flatness, mx, my, (x1+x2)/2.0f, (y1+y2)/2.0f, x2, y2, add_last_point);
+        }
+        else if(add_last_point)
+        {
+            add_vertex(x2, y2, true);
+        }
+    }
+
     std::vector<contour_t> m_contours;
     float m_min_x;
     float m_min_y;
@@ -791,7 +880,6 @@ class parser
 
 auto const html_head = 
 R"html(
-
 <!DOCTYPE html>
 <html>
 <body>
@@ -811,7 +899,6 @@ ctx.beginPath();
 
 auto const html_foot =
 R"html(
-
 ctx.fill();
 
 </script>
@@ -823,7 +910,7 @@ ctx.fill();
 void draw_contour(ttf::shape::contour_t const & c)
 {
     // fmt::print("ctx.beginPath();\n");
-    fmt::print("ctx.moveTo({}, {});\n", c[0].x, c[0].y);
+    fmt::print("\nctx.moveTo({}, {});\n", c[0].x, c[0].y);
 
     auto prev_on_curve = true;
     auto cx = 0;
@@ -886,7 +973,8 @@ int main(int argc, char const * argv[])
         [](auto c) { return static_cast<std::byte>(c); });
 
     auto info = ttf::parser{std::move(contents)};
-    auto const s = info.glyph_shape(info.glyph_index(197));
+    auto const s_b = info.glyph_shape(info.glyph_index(0x262D));
+    auto const s = s_b.flatten(0.5f);
 
     fmt::print(html_head, (s.width()+1)/2, (s.height()+2)/2, -s.min_x(), -s.max_y());
 
